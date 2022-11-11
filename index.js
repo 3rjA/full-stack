57c2197d97e103bdcca86c9ca1977d1b311f6996
3rjA <106434948+3rjA@users.noreply.github.com>
2022-11-11 10:10:34 +0200

    luodaan kansio

--- index.js
+++ index.js
@@ -7,9 +7,15 @@ const person = require('./person');
 console.log(person);
 
 // luodaan tiedosto 
-fs.mkdir(path.join(__dirname, '/testikansio'), {}, err => {
+/*fs.mkdir(path.join(__dirname, '/testikansio'), {}, err => {
     if (err) throw err;
     console.log('Kansio luotu...');
-});
+});*/
 
+// luodaan kansio ja kirjoitetaan siihen 
+
+fs.writeFile(path.join(__dirname, '/testikansio', 'heippa.txt'), 'Terppa maailma!', err => {
+if (err) throw err;
+console.log('kansio luotu...');
+});
 
