af3b5b6598a1aca4184af9475030a6a891ee5415
3rjA <106434948+3rjA@users.noreply.github.com>
2022-11-11 09:43:20 +0200

    tavaraa

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ console.log('Heippahei node js:sta');
 const person = require('./person');
 console.log(person);
 
-// luodaan tiedosto tiedoston sisälle
-FileSystem.mkdir(path.join(__dirname, '/testikansio'), {}, err =>{
+// luodaan tiedosto 
+fs.mkdir(path.join(__dirname, '/testikansio'), {}, err => {
     if (err) throw err;
     console.log('Kansio luotu...');
 });
