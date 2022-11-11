bb6745383a77beeb4a1fbb7dd3164dee5dd138bd
3rjA <106434948+3rjA@users.noreply.github.com>
2022-11-11 09:36:43 +0200

    kansio luotu

--- index.js
+++ index.js
@@ -4,4 +4,10 @@ console.log('Heippahei node js:sta');
 const person = require('./person');
 console.log(person);
 
+// luodaan tiedosto tiedoston sisälle
+FileSystem.mkdir(path.join(__dirname, '/testikansio'), {}, err =>{
+    if (err) throw err;
+    console.log('Kansio luotu...');
+});
+
 
